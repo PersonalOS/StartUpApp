c2ee0d2bbf4a4a41efe090a67726fcd9e76af68e
FelixSun <Felix.SunKJ@outlook.com>
2022-03-02 09:49:40 +0800

    feat:委托测试

---
+++ DelegateTest/DelegateTestClass.cs
@@ -0,0 +1,14 @@
+﻿namespace DelegateTest
+{
+    delegate int Transformer(int x);
+    public class DelegateTestClass
+    {
+        public void TestFunc()
+        {
+            Transformer t = Square;
+            Console.WriteLine(t(3));
+        }
+
+        static int Square(int x) { return x * x; }
+    }
+}
---
+++ StartUp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DelegateTest;
+
+Console.WriteLine("Hello, World!");
+
+//委托测试
+DelegateTestClass delegateTestClass = new DelegateTestClass();
+delegateTestClass.TestFunc();
+
+
